1ae514b4ee6887c025abe11b69f1ca090426ae9c
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-02-12 13:02:29 +0530

    Add files via upload
    Enum constructor are always private

--- enumx.java
+++ enumx.java
@@ -1,17 +1,53 @@
-enum status{
-    Running , Failed , Success, Pending;
-}
+enum Laptop {
+    Dell(20000) , Hp(24000) , Acer , Lenovo(30000) ; 
+    
+    public int price ;
 
-public class enumx {
-    public static void main(String a[]){
-        status[] ss = status.values() ; 
-        for(status s : ss)
-            {
-                System.out.println(s + ": " + s.ordinal());
-            }
-        ;
-        
-
+    private Laptop() {     // 1 times call 
+        price = 50000;
+        System.out.println("Default constructor called, price set to: " + price);
     }
+
+    private Laptop(int price) {
+        System.out.println("Constructor called with price: " + price);
+        this.price = price;
+    }
+
+
+
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    } 
+    
     
 }
+
+
+public class enumx {
+    public static void main(String a[])
+    {
+        for(Laptop lap: Laptop.values()) {
+            System.out.println(lap + ":"+ lap.getPrice());
+    }
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
